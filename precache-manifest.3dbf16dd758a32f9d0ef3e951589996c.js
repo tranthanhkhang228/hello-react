7f86058e5da69c7c722af0a6ccaee5cafebc1ee2
tranthanhkhang2281999 <MrLeo3Q@gmail.com>
2020-03-13 21:16:14 +0700

    Updates

---
+++ precache-manifest.3dbf16dd758a32f9d0ef3e951589996c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516a304b14b15f258fbbb0cd3de52179",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "b673c3679a53620a5201",
+    "url": "/hello-react/static/css/main.f1e522ba.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/hello-react/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hello-react/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b673c3679a53620a5201",
+    "url": "/hello-react/static/js/main.47675b0d.chunk.js"
+  },
+  {
+    "revision": "47ba93f0211ebc2cae1a",
+    "url": "/hello-react/static/js/runtime-main.614da19a.js"
+  }
+]);
